c6e4848e1543b8cd22c4ebb8ac9c6444dd8f9a8d
Trần Tuấn Anh <91252961+anhtt-23810310166@users.noreply.github.com>
2026-06-20 18:33:53 +0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,79 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Task 1: Lấy danh sách tất cả sách sử dụng Async/Await
+public_users.get('/', async function (req, res) {
+  try {
+    const getAllBooks = () => Promise.resolve(books);
+    const bookList = await getAllBooks();
+    return res.status(200).json(bookList);
+  } catch (error) {
+    return res.status(500).json({ message: "Error retrieving books" });
+  }
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Task 2: Lấy thông tin sách dựa trên ISBN sử dụng Async/Await
+public_users.get('/isbn/:isbn', async function (req, res) {
+  try {
+    const isbn = req.params.isbn;
+    const getBookByISBN = (id) => {
+      return new Promise((resolve, reject) => {
+        if (books[id]) resolve(books[id]);
+        else reject("Book not found");
+      });
+    };
+    const book = await getBookByISBN(isbn);
+    return res.status(200).json(book);
+  } catch (error) {
+    return res.status(404).json({ message: error });
+  }
 });
-
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Task 3: Lấy thông tin sách dựa trên Tác giả sử dụng Async/Await
+public_users.get('/author/:author', async function (req, res) {
+  try {
+    const author = req.params.author;
+    const getBooksByAuthor = (authorName) => {
+      return new Promise((resolve) => {
+        const filteredBooks = Object.keys(books)
+          .filter(id => books[id].author.toLowerCase() === authorName.toLowerCase())
+          .map(id => ({ isbn: id, ...books[id] }));
+        resolve(filteredBooks);
+      });
+    };
+    const results = await getBooksByAuthor(author);
+    return res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ message: "Error parsing data" });
+  }
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Task 4: Lấy thông tin sách dựa trên Tiêu đề sử dụng Async/Await
+public_users.get('/title/:title', async function (req, res) {
+  try {
+    const title = req.params.title;
+    const getBooksByTitle = (bookTitle) => {
+      return new Promise((resolve) => {
+        const filteredBooks = Object.keys(books)
+          .filter(id => books[id].title.toLowerCase() === bookTitle.toLowerCase())
+          .map(id => ({ isbn: id, ...books[id] }));
+        resolve(filteredBooks);
+      });
+    };
+    const results = await getBooksByTitle(title);
+    return res.status(200).json(results);
+  } catch (error) {
+    return res.status(500).json({ message: "Error parsing data" });
+  }
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Lấy thông tin review sách
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn].reviews);
+  }
+  return res.status(404).json({ message: "Book not found" });
 });
 
-module.exports.general = public_users;
+module.exports = public_users;
